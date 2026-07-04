b178492f2abaf4f221e4fc57e4f18217ab0c9873
bawabethayah <researchandrenal@gmail.com>
2026-07-04 14:32:52 +0300

    Update siteData.ts

--- data/siteData.ts
+++ data/siteData.ts
@@ -1,1 +1,14 @@
+export const languages = ["English", "Arabic", "French", "Spanish"];
 
+export const topics = [
+  "Chronic Kidney Disease (CKD)",
+  "Acute Kidney Injury (AKI)",
+  "Dialysis",
+  "Glomerular Diseases",
+  "Kidney Transplantation",
+  "Hypertension",
+  "Electrolytes",
+  "Onco-Nephrology",
+];
+
+export const socials = ["YouTube", "LinkedIn", "Facebook", "Instagram", "X", "Threads"];
